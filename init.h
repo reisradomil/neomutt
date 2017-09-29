d9ba40ccc57e9fa1a940655718080b52b7696f6b
Pietro Ceruti <gahr@gahr.ch>
2017-09-29 22:52:04 +0000

    Move setting definition where the doc parser can pick it up
    Issue #802

--- init.h
+++ init.h
@@ -4394,16 +4394,16 @@ struct Option MuttVars[] = {
   ** .pp
   ** When \fIset\fP, NeoMutt will collapse all threads when entering a folder.
   */
-  /*--*/
-  { "pgp_encrypt_self",   DT_QUAD, R_NONE, OPT_PGP_ENCRYPT_SELF,   MUTT_NO },
-  { "smime_encrypt_self", DT_QUAD, R_NONE, OPT_SMIME_ENCRYPT_SELF, MUTT_NO },
-
   { "show_notifications", DT_BOOL, R_NONE, OPT_SHOW_NOTIFICATIONS, MUTT_NO },
   /*
    ** .pp
    ** When \fIset\fP, and if there is any notification, NeoMutt will show the
    ** notifications screen on startup.
    */
+
+  /*--*/
+  { "pgp_encrypt_self",   DT_QUAD, R_NONE, OPT_PGP_ENCRYPT_SELF,   MUTT_NO },
+  { "smime_encrypt_self", DT_QUAD, R_NONE, OPT_SMIME_ENCRYPT_SELF, MUTT_NO },
 
   { NULL, 0, 0, 0, 0 },
 };
