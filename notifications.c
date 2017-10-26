neomutt/neomutt-old

Let notifications have a body too
I am confidend I am using the pager wrong. Especially, there should be a
way to display a buffer in the pager without having to dump the contents
to a temporary file first. Also, I will have to figure out how to
pretty-format the contents of the notifications.

Issue #802

notifications.c
@@ -21,6 +21,7 @@
  */
 
 #include "config.h"
+#include "mutt/file.h"
 #include "mutt/mapping.h"
 #include "mutt/memory.h"
 #include "mutt/message.h"
@@ -32,8 +33,8 @@
 #include "keymap.h"
 #include "mutt_menu.h"
 #include "opcodes.h"
-#include <string.h>
-
+#include "pager.h"
+#include "protos.h"
 #include <string.h>
 
 static TAILQ_HEAD(NotificationsHead,
@@ -41,15 +42,16 @@ static TAILQ_HEAD(NotificationsHead,
 
 struct Notification
 {
-  const char *data;
+  const char *head;
+  const char *body;
   TAILQ_ENTRY(Notification) entries;
 };
 
 static const struct Mapping NotificationsHelp[] = {
-  { N_("Exit"), OP_EXIT }, { NULL, 0 },
+  { N_("View"), OP_VIEW_ATTACH }, { N_("Exit"), OP_EXIT }, { NULL, 0 },
 };
 
-static void notifications_entry(char *b, size_t blen, struct Menu *menu, int num)
+static struct Notification *notifications_select(int num)
 {
   size_t i = 0;
   struct Notification *np;
@@ -60,11 +62,41 @@ static void notifications_entry(char *b, size_t blen, struct Menu *menu, int num
       break;
     }
   }
+  return np;
+}
+
+static void notifications_entry(char *b, size_t blen, struct Menu *menu, int num)
+{
+  const struct Notification *np = notifications_select(num);
   if (!np)
   {
     return;
   }
-  snprintf(b, blen, "%s", np->data);
+
+  snprintf(b, blen, "%3d - %s", num + 1 /* 0-based */, np->head);
+}
+
+static void notifications_view(int num)
+{
+  const struct Notification *np = notifications_select(num);
+  if (!np || !np->body)
+  {
+    return;
+  }
+
+  char tempfile[_POSIX_PATH_MAX];
+  mutt_mktemp(tempfile, sizeof(tempfile));
+  FILE *fp = mutt_file_fopen(tempfile, "w");
+  if (!fp)
+  {
+    mutt_perror(_("Failure to open temp file."));
+    return;
+  }
+
+  fprintf(fp, "%s\n\n%s", np->head, np->body);
+  mutt_file_fclose(&fp);
+  mutt_pager(np->head, tempfile, true, NULL);
+  mutt_file_unlink(tempfile);
 }
 
 void mutt_notifications_show(void)
@@ -97,6 +129,10 @@ void mutt_notifications_show(void)
       op = mutt_menu_loop(menu);
     switch (op)
     {
+      case OP_VIEW_ATTACH:
+        notifications_view(menu->current);
+        menu->redraw = REDRAW_FULL;
+        break;
       case OP_EXIT:
         mutt_pop_current_menu(menu);
         mutt_menu_destroy(&menu);
@@ -110,21 +146,23 @@ void mutt_notifications_show(void)
   /* not reached */
 }
 
-void mutt_notifications_add(const char *s)
+void mutt_notifications_add(const char *head, const char *body, bool copy)
 {
-  if (!s || !*s)
+  if (!head || !*head)
     return;
 
   struct Notification *np;
   TAILQ_FOREACH(np, &Notifications, entries)
   {
-    if (strcmp(s, np->data) == 0)
+    if (strcmp(head, np->head) == 0)
     {
       return;
     }
   }
+
   np = mutt_mem_calloc(1, sizeof(struct Notification));
-  np->data = mutt_str_strdup(s);
+  np->head = mutt_str_strdup(head);
+  np->body = copy ? mutt_str_strdup(body) : body;
   TAILQ_INSERT_TAIL(&Notifications, np, entries);
 }
 

reisradomil/neomutt

notifications.c
@@ -21,6 +21,7 @@
  */
 
 #include "config.h"
+#include "lib/file.h"
 #include "lib/mapping.h"
 #include "lib/memory.h"
 #include "lib/message.h"
@@ -32,6 +33,8 @@
 #include "keymap.h"
 #include "mutt_menu.h"
 #include "opcodes.h"
+#include "pager.h"
+#include "protos.h"
 
 #include <string.h>
 
@@ -40,15 +43,16 @@ static TAILQ_HEAD(NotificationsHead,
 
 struct Notification
 {
-  const char *data;
+  const char *head;
+  const char *body;
   TAILQ_ENTRY(Notification) entries;
 };
 
 static const struct Mapping NotificationsHelp[] = {
-  { N_("Exit"), OP_EXIT }, { NULL, 0 },
+  { N_("View"), OP_VIEW_ATTACH }, { N_("Exit"), OP_EXIT }, { NULL, 0 },
 };
 
-static void notifications_entry(char *b, size_t blen, struct Menu *menu, int num)
+static struct Notification *notifications_select(int num)
 {
   size_t i = 0;
   struct Notification *np;
@@ -59,11 +63,41 @@ static void notifications_entry(char *b, size_t blen, struct Menu *menu, int num
       break;
     }
   }
+  return np;
+}
+
+static void notifications_entry(char *b, size_t blen, struct Menu *menu, int num)
+{
+  const struct Notification *np = notifications_select(num);
   if (!np)
   {
     return;
   }
-  snprintf(b, blen, "%s", np->data);
+
+  snprintf(b, blen, "%3d - %s", num + 1 /* 0-based */, np->head);
+}
+
+static void notifications_view(int num)
+{
+  const struct Notification *np = notifications_select(num);
+  if (!np || !np->body)
+  {
+    return;
+  }
+
+  char tempfile[_POSIX_PATH_MAX];
+  mutt_mktemp(tempfile, sizeof(tempfile));
+  FILE *fp = safe_fopen(tempfile, "w");
+  if (!fp)
+  {
+    mutt_perror(_("Failure to open temp file."));
+    return;
+  }
+
+  fprintf(fp, "%s\n\n%s", np->head, np->body);
+  safe_fclose(&fp);
+  mutt_pager(np->head, tempfile, true, NULL);
+  mutt_unlink(tempfile);
 }
 
 void mutt_notifications_show(void)
@@ -96,6 +130,10 @@ void mutt_notifications_show(void)
       op = mutt_menu_loop(menu);
     switch (op)
     {
+      case OP_VIEW_ATTACH:
+        notifications_view(menu->current);
+        menu->redraw = REDRAW_FULL;
+        break;
       case OP_EXIT:
         mutt_pop_current_menu(menu);
         mutt_menu_destroy(&menu);
@@ -109,21 +147,23 @@ void mutt_notifications_show(void)
   /* not reached */
 }
 
-void mutt_notifications_add(const char *s)
+void mutt_notifications_add(const char *head, const char *body, bool copy)
 {
-  if (!s || !*s)
+  if (!head || !*head)
     return;
 
   struct Notification *np;
   TAILQ_FOREACH(np, &Notifications, entries)
   {
-    if (strcmp(s, np->data) == 0)
+    if (strcmp(head, np->head) == 0)
     {
       return;
     }
   }
+
   np = safe_calloc(1, sizeof(struct Notification));
-  np->data = safe_strdup(s);
+  np->head = safe_strdup(head);
+  np->body = copy ? safe_strdup(body) : body;
   TAILQ_INSERT_TAIL(&Notifications, np, entries);
 }
 